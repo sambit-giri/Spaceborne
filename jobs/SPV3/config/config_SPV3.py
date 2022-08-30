🐛BUGFIX: sigma_eps2 had the wrong value (in the config file). Now EP is ok ✅

jobs/SPV3/config/config_SPV3.py
@@ -40,7 +40,7 @@ covariance_config = {
     # this is the one used by me, Vincenzo and CLOE. The blocks in the 2D covmat will be indexed by ell1, ell2
     'block_index': 'ell',
     'which_probe_response': 'variable',
-    'sigma_eps2': (0.3 * np.sqrt(2)) ** 2,  # ! new
+    'sigma_eps2': (0.26 * np.sqrt(2)) ** 2,  # ! new
     'ng': 28.73,  # ! new
     'save_covariance': True,
     'save_covariance_dat': True,
@@ -50,8 +50,8 @@ Sijkl_config = {
     'input_WF': 'vincenzo_SPV3',
     'WF_normalization': 'IST',
     'has_IA': True,  # whether to include IA in the WF used to compute Sijkl
-    'use_precomputed_sijkl': True,
-    'save_Sijkl': False,
+    'use_precomputed_sijkl': False,
+    'save_Sijkl': True,
 }
 
 FM_config = {
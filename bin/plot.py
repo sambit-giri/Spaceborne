305c5a0e55e85d45e07cfd55b26a44a0d00b5ca3
Davide Sciotti <davidesciotti@gmail.com>
2022-09-28 17:42:23 +0200

    updated bin/plot.py but it's very job-specific. Also, it doesn't seem to work

--- bin/plot.py
+++ bin/plot.py
@@ -11,8 +11,7 @@ from getdist import MCSamples, plots
 from matplotlib.cm import get_cmap
 from getdist.gaussian_mixtures import GaussianND
 
-project_path = Path.cwd().parent.parent.parent
-job_path = Path.cwd().parent
+project_path = Path.cwd().parent
 
 sys.path.append(str(project_path.parent / 'common_data'))
 import common_lib.my_module as mm
@@ -22,25 +21,16 @@ import common_config.ISTF_fid_params as ISTF_fid
 sys.path.append(str(project_path / 'bin'))
 import plots_FM_running as plot_utils
 
-sys.path.append(str(project_path / 'jobs'))
-import SSC_comparison.config.config_SSC_comparison as cfg
-
-
 # plot config
 matplotlib.rcParams.update(mpl_cfg.mpl_rcParams_dict)
 matplotlib.use('Qt5Agg')
 markersize = 10
 
-nbl = cfg.general_config['nbl']
-
-# import all outputs from this script
-FM_dict = dict(mm.get_kv_pairs(job_path / 'output/FM', filetype="txt"))
-FM_dict_PyCCL = dict(mm.get_kv_pairs(job_path.parent / 'PyCCL_forecast/output/FM', filetype="txt"))
-FM_dict = {**FM_dict, **FM_dict_PyCCL}
+########################################################################################################################
 
 # ! options
 GO_or_GS = 'GS'
-probe = '3x2pt'
+probe = 'WL'
 which_comparison = 'GO_vs_GS'  # this is just to set the title of the plot
 which_Rl = 'var'
 which_uncertainty = 'marginal'
@@ -48,7 +38,17 @@ ell_max_WL = 5000
 ell_max_GC = 3000
 nparams = 7
 zbins = 10
+job = 'SPV3'
+model = 'flat'
 # ! end options
+
+job_path = project_path / f'jobs/{job}'
+
+# TODO fix this
+if job == 'SPV3':
+    nbl = 32
+else:
+    raise ValueError
 
 # fiducial values
 fid_cosmo = np.asarray([ISTF_fid.primary[key] for key in ISTF_fid.primary.keys()])[:7]
@@ -59,11 +59,15 @@ assert GO_or_GS == 'GS', 'GO_or_GS should be GS, if not what are you comparing?'
 
 if probe == '3x2pt':
     probe_lmax = 'XC'
+    probe_folder = 'All'
+    probename_vinc = probe
     param_names_label = mpl_cfg.general_dict['param_names_label_rm'] + mpl_cfg.general_dict['IA_names_label'] + \
                         mpl_cfg.general_dict['bias_names_label']
     fid = np.concatenate((fid_cosmo, fid_IA, fid_bias), axis=0)
 else:
     probe_lmax = probe
+    probe_folder = probe + 'O'
+    probename_vinc = probe + 'O'
 
 if probe == 'WL':
     ell_max = ell_max_WL
@@ -79,13 +83,47 @@ if probe == 'GC':
 elif probe not in ['WL', 'GC', '3x2pt']:
     raise ValueError('probe should be WL, GC or 3x2pt')
 
+# import vincenzo's FM, not in a dictionary because they are all split into different folders
+vinc_FM_folder = 'vincenzo/SPV3_07_2022/FishMat'
+specs = f'NonFlat-GR-TB-idMag0-idRSD0-idFS0-idSysWL3-idSysGC4-EP{zbins}'
+
+# TODO pessimistic case
+FM_GO = np.genfromtxt(
+    project_path.parent / f'common_data/{vinc_FM_folder}/GaussOnly/{probe_folder}/OneSample/fm-{probename_vinc}-{nbl}-wzwaCDM-{specs}.dat')
+FM_GS = np.genfromtxt(
+    project_path.parent / f'common_data/{vinc_FM_folder}/GaussSSC/{probe_folder}/OneSample/fm-{probename_vinc}-{nbl}-wzwaCDM-{specs}.dat')
+
+if model == 'flat':
+    FM_GO = np.delete(FM_GO, 1, 0)
+    FM_GO = np.delete(FM_GO, 1, 1)
+    FM_GS = np.delete(FM_GS, 1, 0)
+    FM_GS = np.delete(FM_GS, 1, 1)
+elif model == 'nonflat':
+    # TODO test this
+    np.insert(arr=fid, obj=1, values=ISTF_fid.extensions['Om_Lambda0'], axis=0)
+    np.insert(arr=param_names_label, obj=1, values='$\Omega_{\rm DE}$', axis=0)
+else:
+    raise ValueError('model must be either flat or nonflat')
+
 fid = fid[:nparams]
 param_names_label = param_names_label[:nparams]
 
-keys = [f'FM_{probe}_GO_lmax{probe_lmax}{ell_max}_nbl{nbl}',
-        f'FM_{probe}_{GO_or_GS}_lmax{probe_lmax}{ell_max}_nbl{nbl}_Rl{which_Rl}',
-        # f'FM_{probe}_{GO_or_GS}_lmax{probe_lmax}{ell_max}_nbl{nbl}_PyCCLKiDS1000',
-        ]
+# find null rows and columns
+zero_rows_idxs = np.where(np.all(FM_GO == 0, axis=0))[0]
+zero_cols_idxs = np.where(np.all(FM_GO == 0, axis=1))[0]
+assert np.array_equal(zero_rows_idxs, zero_cols_idxs), 'null rows and columns indices should be the same!'
+
+# print the corresponding indices
+if zero_rows_idxs.shape[0] != 0:
+    print(f'FM had some null rows and columns, at indices {zero_rows_idxs}')
+
+# delete the null rows and columns
+FM_GO = np.delete(FM_GO, zero_rows_idxs, axis=0)
+FM_GO = np.delete(FM_GO, zero_rows_idxs, axis=1)
+FM_GS = np.delete(FM_GS, zero_rows_idxs, axis=0)
+FM_GS = np.delete(FM_GS, zero_rows_idxs, axis=1)
+
+########################################################################################################################
 
 label_list = [f'Gauss-only covmat (GO)',
               # f'{GO_or_GS} Rlconst',
@@ -96,11 +134,11 @@ label_list = [f'Gauss-only covmat (GO)',
 
 data = []
 fom = {}
-for i, key in enumerate(keys):
-    uncert = np.asarray(mm.uncertainties_FM(FM_dict[key], nparams=nparams, fiducials=fid,
+for FM, case in zip([FM_GO, FM_GS], ('GO', 'GS')):
+    uncert = np.asarray(mm.uncertainties_FM(FM, nparams=nparams, fiducials=fid,
                                             which_uncertainty=which_uncertainty, normalize=True)[:nparams])
     data.append(uncert)
-    fom[key] = mm.compute_FoM(FM_dict[key])
+    fom[case] = mm.compute_FoM(FM)
 
 # compute percent diff of the cases chosen - careful of the indices!
 print('careful about this absolute value!')
@@ -119,15 +157,14 @@ else:
 plot_utils.bar_plot(data, title, label_list, nparams=nparams, param_names_label=param_names_label, bar_width=0.18,
                     second_axis=True)
 if probe == '3x2pt':
-    plot_utils.triangle_plot(FM_dict[keys[0]], FM_dict[keys[1]], fiducials=fid,
+    plot_utils.triangle_plot(FM_GO, FM_GS, fiducials=fid,
                              title=title, param_names_label=param_names_label)
 
 plt.savefig(job_path / f'output/plots/{which_comparison}/'
                        f'{probe}_ellmax{ell_max}_Rl{which_Rl}_{which_uncertainty}.png')
 
 # compute and print FoM
-print('GO FoM:', mm.compute_FoM(FM_dict[f'FM_{probe}_GO_lmax{probe_lmax}{ell_max}_nbl{nbl}']))
-print('Rl const FoM:', mm.compute_FoM(FM_dict[f'FM_{probe}_{GO_or_GS}_lmax{probe_lmax}{ell_max}_nbl{nbl}_Rlconst']))
-print('Rl var FoM:', mm.compute_FoM(FM_dict[f'FM_{probe}_{GO_or_GS}_lmax{probe_lmax}{ell_max}_nbl{nbl}_Rlvar']))
+print('GO FoM:', mm.compute_FoM(FM_GO))
+print('Rl var FoM:', mm.compute_FoM(FM_GS))
 
 print('*********** done ***********')

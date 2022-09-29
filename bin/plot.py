3042acbda7061852ebd4d87827d6ae5c92197b87
Davide Sciotti <davidesciotti@gmail.com>
2022-09-29 14:29:44 +0200

    bug fixed: I had to remove the new nuisance parameters from the FM to get constraints close to the ISTF ones

--- bin/plot.py
+++ bin/plot.py
@@ -30,7 +30,7 @@ markersize = 10
 
 # ! options
 GO_or_GS = 'GS'
-probe = 'WL'
+probe = '3x2pt'
 which_comparison = 'GO_vs_GS'  # this is just to set the title of the plot
 which_Rl = 'var'
 which_uncertainty = 'marginal'
@@ -38,14 +38,18 @@ ell_max_WL = 5000
 ell_max_GC = 3000
 nparams = 7
 zbins = 10
-job = 'SPV3'
+which_job = 'SPV3'
 model = 'flat'
+which_diff = 'normal'
+check_old_FM = True
+fix_dz_nuisance = True  # whether to remove the rows/cols for the dz nuisance parameters (ie whether to fix them)
+fix_shear_bias = True  # whether to remove the rows/cols for the shear bias nuisance parameters (ie whether to fix them)
 # ! end options
 
-job_path = project_path / f'jobs/{job}'
+job_path = project_path / f'jobs/{which_job}'
 
 # TODO fix this
-if job == 'SPV3':
+if which_job == 'SPV3':
     nbl = 32
 else:
     raise ValueError
@@ -56,6 +60,12 @@ fid_IA = np.asarray([ISTF_fid.IA_free[key] for key in ISTF_fid.IA_free.keys()])
 fid_bias = np.asarray([ISTF_fid.photoz_bias[key] for key in ISTF_fid.photoz_bias.keys()])
 
 assert GO_or_GS == 'GS', 'GO_or_GS should be GS, if not what are you comparing?'
+assert which_diff in ['normal', 'mean'], 'which_diff should be "normal" or "mean"'
+assert which_uncertainty in ['marginal', 'conditional'], 'which_uncertainty should be "marginal" or "conditional"'
+assert which_Rl in ['const', 'var'], 'which_Rl should be "const" or "var"'
+assert model in ['flat', 'nonflat'], 'model should be "flat" or "nonflat"'
+assert probe in ['WL', 'GC', '3x2pt'], 'probe should be "WL" or "GC" or "3x2pt"'
+assert which_job == 'SPV3', 'which_job should be "SPV3"'
 
 if probe == '3x2pt':
     probe_lmax = 'XC'
@@ -80,9 +90,6 @@ if probe == 'GC':
     param_names_label = mpl_cfg.general_dict['param_names_label_rm'] + mpl_cfg.general_dict['bias_names_label']
     fid = np.concatenate((fid_cosmo, fid_bias), axis=0)
 
-elif probe not in ['WL', 'GC', '3x2pt']:
-    raise ValueError('probe should be WL, GC or 3x2pt')
-
 # import vincenzo's FM, not in a dictionary because they are all split into different folders
 vinc_FM_folder = 'vincenzo/SPV3_07_2022/FishMat'
 specs = f'NonFlat-GR-TB-idMag0-idRSD0-idFS0-idSysWL3-idSysGC4-EP{zbins}'
@@ -93,35 +100,40 @@ FM_GO = np.genfromtxt(
 FM_GS = np.genfromtxt(
     project_path.parent / f'common_data/{vinc_FM_folder}/GaussSSC/{probe_folder}/OneSample/fm-{probename_vinc}-{nbl}-wzwaCDM-{specs}.dat')
 
+mm.matshow(FM_GO, log=True, title=f'{probe}, before cuts')
+# remove rows/cols for the redshift center nuisance parameters
+if fix_dz_nuisance:
+    FM_GO = FM_GO[:-10, :-10]
+    FM_GS = FM_GS[:-10, :-10]
+
+if probe != 'GC':
+    if fix_shear_bias:
+        assert fix_dz_nuisance, 'the case with free dz_nuisance is not implemented (you just need to be more careful with the slicing)'
+        FM_GO = FM_GO[:-10, :-10]
+        FM_GS = FM_GS[:-10, :-10]
+
+
 if model == 'flat':
-    FM_GO = np.delete(FM_GO, 1, 0)
-    FM_GO = np.delete(FM_GO, 1, 1)
-    FM_GS = np.delete(FM_GS, 1, 0)
-    FM_GS = np.delete(FM_GS, 1, 1)
+    FM_GO = np.delete(FM_GO, obj=1, axis=0)
+    FM_GO = np.delete(FM_GO, obj=1, axis=1)
+    FM_GS = np.delete(FM_GS, obj=1, axis=0)
+    FM_GS = np.delete(FM_GS, obj=1, axis=1)
 elif model == 'nonflat':
-    # TODO test this
-    np.insert(arr=fid, obj=1, values=ISTF_fid.extensions['Om_Lambda0'], axis=0)
-    np.insert(arr=param_names_label, obj=1, values='$\Omega_{\rm DE}$', axis=0)
-else:
-    raise ValueError('model must be either flat or nonflat')
+    nparams += 1
+    fid = np.insert(arr=fid, obj=1, values=ISTF_fid.extensions['Om_Lambda0'], axis=0)
+    param_names_label = np.insert(arr=param_names_label, obj=1, values='$\\Omega_{\\rm DE}$', axis=0)
 
 fid = fid[:nparams]
 param_names_label = param_names_label[:nparams]
 
-# find null rows and columns
-zero_rows_idxs = np.where(np.all(FM_GO == 0, axis=0))[0]
-zero_cols_idxs = np.where(np.all(FM_GO == 0, axis=1))[0]
-assert np.array_equal(zero_rows_idxs, zero_cols_idxs), 'null rows and columns indices should be the same!'
 
-# print the corresponding indices
-if zero_rows_idxs.shape[0] != 0:
-    print(f'FM had some null rows and columns, at indices {zero_rows_idxs}')
-
-# delete the null rows and columns
-FM_GO = np.delete(FM_GO, zero_rows_idxs, axis=0)
-FM_GO = np.delete(FM_GO, zero_rows_idxs, axis=1)
-FM_GS = np.delete(FM_GS, zero_rows_idxs, axis=0)
-FM_GS = np.delete(FM_GS, zero_rows_idxs, axis=1)
+# remove null rows and columns
+idx = mm.find_null_rows_cols_2D(FM_GO)
+idx_GS = mm.find_null_rows_cols_2D(FM_GS)
+assert np.array_equal(idx, idx_GS), 'the null rows/cols indices should be equal for GO and GS'
+FM_GO = mm.remove_null_rows_cols_2D(FM_GO, idx)
+FM_GS = mm.remove_null_rows_cols_2D(FM_GS, idx)
+mm.matshow(FM_GO, log=True)
 
 ########################################################################################################################
 
@@ -130,22 +142,40 @@ label_list = [f'Gauss-only covmat (GO)',
               f'Gauss+SS covmat ({GO_or_GS})',
               # f'{GO_or_GS} PyCCL',
               # f'% diff wrt mean, Rlconst vs PyCCL',
-              f'[(GS/GO - 1) $\\times$ 100]']
+              f'[(GS/GO - 1) $\\times$ 100]',
+              'bing',
+              'pluf'
+              ]
 
 data = []
 fom = {}
 for FM, case in zip([FM_GO, FM_GS], ('GO', 'GS')):
     uncert = np.asarray(mm.uncertainties_FM(FM, nparams=nparams, fiducials=fid,
-                                            which_uncertainty=which_uncertainty, normalize=True)[:nparams])
+                                            which_uncertainty=which_uncertainty, normalize=True))
     data.append(uncert)
     fom[case] = mm.compute_FoM(FM)
 
+if check_old_FM:
+    probe_lmax = probe
+    if probe == '3x2pt':
+        probe_lmax = 'XC'
+    FM_GO_test = np.genfromtxt(
+        f'/Users/davide/Documents/Lavoro/Programmi/SSC_restructured_v2/jobs/SSC_comparison/output/FM/FM_{probe}_GO_lmax{probe_lmax}{ell_max}_nbl30.txt')
+
+    uncert = np.asarray(mm.uncertainties_FM(FM_GO_test, nparams=nparams, fiducials=fid,
+                                            which_uncertainty=which_uncertainty, normalize=True))
+    data.append(uncert)
+
+# ! delete until nparams!!
 # compute percent diff of the cases chosen - careful of the indices!
-print('careful about this absolute value!')
-diff_1 = mm.percent_diff(data[-1], data[-2])
-# diff_2 = mm.percent_diff_mean(data[-2], data[-1])
-data.append(diff_1)
-# data.append(diff_2)
+# print('careful about this absolute value!')
+# if which_diff == 'normal':
+#     diff_funct = mm.percent_diff
+# else:
+#     diff_funct = mm.percent_diff_mean
+#
+# diff = diff_funct(data[-1], data[-2])
+# data.append(diff)
 
 data = np.asarray(data)
 
@@ -155,7 +185,10 @@ else:
     title = 'FM normalized 1-$\\sigma$ parameter constraints, %s - lower is better' % probe  # for PhD workshop
 
 plot_utils.bar_plot(data, title, label_list, nparams=nparams, param_names_label=param_names_label, bar_width=0.18,
-                    second_axis=True)
+                    second_axis=False)
+
+assert 1 > 2
+
 if probe == '3x2pt':
     plot_utils.triangle_plot(FM_GO, FM_GS, fiducials=fid,
                              title=title, param_names_label=param_names_label)

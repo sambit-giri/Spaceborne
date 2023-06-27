1fa3adf0e0ac72c530c34f7aa1d2adac7ae45ec2
Davide Sciotti <davidesciotti@gmail.com>
2023-06-27 21:56:50 +0200

    cleanup: removed old way of computing the percent diff

--- jobs/SSC_paper_final/bin/plot_constraints_updated.py
+++ jobs/SSC_paper_final/bin/plot_constraints_updated.py
@@ -32,9 +32,14 @@ fix_galaxy_bias = False
 fix_shear_bias = False
 fix_dz = True
 include_fom = True
-shear_bias_prior = 1e-4
+fid_shear_bias_prior = 1e-4
+shear_bias_priors = [.5e-4, 5e-4, 50e-4]
+shear_bias_prior = .5e-4
+galaxy_bias_perc_priors = [0.001, 1, 1000]  # = sigma_b / b, with sigma_b the prior and b the fiducial value
 add_shear_bias_prior = True
-string_columns = ['probe', 'go_or_gs', 'fix_shear_bias', 'add_shear_bias_prior', 'shear_bias_prior']
+add_galaxy_bias_prior = True
+string_columns = ['probe', 'go_or_gs', 'fix_shear_bias', 'add_shear_bias_prior', 'shear_bias_prior',
+                  'galaxy_bias_perc_prior']
 # ! options
 
 
@@ -43,13 +48,14 @@ go_or_gs_folder_dict = {
     'GS': 'GaussSSC',
 }
 probes_vinc = ('WLO', 'GCO', '3x2pt')
-probes_vinc = ('3x2pt',)
+probes_vinc = ('WLO', 'GCO',)
 
 fm_uncert_df = pd.DataFrame()
 for go_or_gs in ['GO', 'GS']:
-    for fix_shear_bias in [False, True]:
+    for fix_galaxy_bias in [False, True]:
         for probe_vinc in probes_vinc:
-            for shear_bias_prior in [.5e-4, 5e-4, 50e-4]:
+            # for shear_bias_prior in shear_bias_priors:
+            for galaxy_bias_perc_prior in galaxy_bias_perc_priors:
 
                 names_params_to_fix = []
 
@@ -93,11 +99,15 @@ for go_or_gs in ['GO', 'GS']:
                     fm_prior_shear_bias = np.zeros(fm.shape)
                     fm_prior_shear_bias[shear_bias_idxs, shear_bias_idxs] = 1 / shear_bias_prior
                     fm += fm_prior_shear_bias
-                # if add_galaxy_bias_prior and not fix_galaxy_bias and probe_vinc != 'WLO':
-                #     galaxy_bias_idxs = [param_names.index(f'b{(zi + 1):02d}_photo') for zi in range(zbins)]
-                #     fm_prior_galaxy_bias = np.zeros(fm.shape)
-                #     fm_prior_galaxy_bias[galaxy_bias_idxs, galaxy_bias_idxs] = 1 / galaxy_bias_prior
-                #     fm += fm_prior_galaxy_bias
+                if add_galaxy_bias_prior and not fix_galaxy_bias and probe_vinc != 'WLO':
+                    # go from sigma_b / b_fid to sigma_b
+                    galaxy_bias_idxs = [param_names.index(f'b{(zi + 1):02d}_photo') for zi in range(zbins)]
+                    galaxy_bias_fid_values = np.array(list(fiducials_dict_trimmed.values()))[galaxy_bias_idxs]
+                    galaxy_bias_prior = galaxy_bias_perc_prior * galaxy_bias_fid_values
+
+                    fm_prior_galaxy_bias = np.zeros(fm.shape)
+                    fm_prior_galaxy_bias[galaxy_bias_idxs, galaxy_bias_idxs] = 1 / galaxy_bias_prior
+                    fm += fm_prior_galaxy_bias
 
                 uncert_fm = mm.uncertainties_fm_v2(fm, fiducials_dict_trimmed, which_uncertainty='marginal',
                                                    normalize=True,
@@ -112,7 +122,8 @@ for go_or_gs in ['GO', 'GS']:
 
                 # this is a list of lists just to have a 'row list' instead of a 'column list',
                 # I still haven't figured out the problem...
-                df_columns_values = [[probe_vinc, go_or_gs, fix_shear_bias, add_shear_bias_prior, shear_bias_prior] +
+                df_columns_values = [[probe_vinc, go_or_gs, fix_shear_bias,
+                                      add_shear_bias_prior, shear_bias_prior, galaxy_bias_perc_prior] +
                                      uncert_fm.tolist() + [fom]]
 
                 # assert False
@@ -132,52 +143,20 @@ perc_diff_df['go_or_gs'] = 'perc_diff'
 perc_diff_df.iloc[:, -1] = (arr_go[:, -1] / arr_gs[:, -1] - 1) * 100
 fm_uncert_df = pd.concat([fm_uncert_df, perc_diff_df], axis=0, ignore_index=True)
 
-# assert False
-
-"""
-# Calculate the percentage difference
-result_df = ((df_gs.iloc[:, 3:] / df_go.iloc[:, 3:]) - 1) * 100
-
-# compute percent differences between GO and GS
-for probe_vinc in probes_vinc:
-    for fix_shear_bias in [True, False]:
-        uncert_go = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GO') &
-                                 (fm_uncert_df['probe'] == probe_vinc) &
-                                 (fm_uncert_df['fix_shear_bias'] == fix_shear_bias)].iloc[:, len(string_columns):].values[0]
-        uncert_gs = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GS') &
-                                 (fm_uncert_df['probe'] == probe_vinc) &
-                                 (fm_uncert_df['fix_shear_bias'] == fix_shear_bias)].iloc[:, len(string_columns):].values[0]
-
-        diff = mm.percent_diff(uncert_gs, uncert_go)
-
-        df_columns_values = [[probe_vinc, 'perc_diff', fix_shear_bias, add_shear_bias_prior, shear_bias_prior] +
-                             diff.tolist()]
-
-        new_df_row = pd.DataFrame(df_columns_values, columns=df_columns_names)
-
-        # the FoM is instead GO/GS-1
-        fom_go = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GO') & (fm_uncert_df['probe'] == probe_vinc)]['FoM'].values[
-            0]
-        fom_gs = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GS') & (fm_uncert_df['probe'] == probe_vinc)]['FoM'].values[
-            0]
-        new_df_row['FoM'] = mm.percent_diff(fom_go, fom_gs)
-
-        fm_uncert_df = pd.concat([fm_uncert_df, new_df_row], ignore_index=True)
-"""
-
 ylabel = r'$(\sigma_{\rm GS}/\sigma_{\rm G} - 1) \times 100$ [%]'
-# data = fm_uncert_df[fm_uncert_df['go_or_gs'] == 'perc_diff'].select_dtypes(include=np.number).values  # all 3 probes
-# label_list = ('WL', 'GCph', r'$3\times 2$pt')
 
 # shorten the dataframe to only one probe and one type of uncertainty (perc_diff)
-fm_uncert_df_short = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GS') &
-                                  (fm_uncert_df['probe'] == '3x2pt') & (fm_uncert_df['fix_shear_bias'] == False)
-                                  ]
+fm_uncert_df_toplot = fm_uncert_df[(fm_uncert_df['go_or_gs'] == 'GO') &
+                                   (fm_uncert_df['probe'] == 'GCO') &
+                                   (fm_uncert_df['fix_shear_bias'] == False)
+                                   ]
 
-data = fm_uncert_df_short.iloc[:, len(string_columns):].values
-label_list = [f'fix_shear_bias={fix_shear_bias}; shear_bias={shear_bias:02f}' for fix_shear_bias, shear_bias in
-              zip(fm_uncert_df_short['fix_shear_bias'].values,
-                  fm_uncert_df_short['shear_bias_prior'].values)]
+data = fm_uncert_df_toplot.iloc[:, len(string_columns):].values
+label_list = [f'fix_shear_bias={fix_shear_bias}; shear_bias_prior={shear_bias:02f}' for fix_shear_bias, shear_bias in
+              zip(fm_uncert_df_toplot['fix_shear_bias'].values,
+                  fm_uncert_df_toplot['shear_bias_prior'].values)]
+label_list = [f'galaxy_bias_perc_prior={galaxy_bias_perc_prior:02f}' for galaxy_bias_perc_prior in
+              fm_uncert_df_toplot['galaxy_bias_perc_prior'].values]
 title = None
 
 if include_fom:

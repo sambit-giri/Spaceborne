80078880584ddc8ae544c905433e8997616d6e45
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-07-25 16:30:10 +0200

    added check

--- spaceborne/config_checker.py
+++ spaceborne/config_checker.py
@@ -29,6 +29,9 @@ class SpaceborneConfigChecker:
     
     def check_which_forecast(self):
         assert self.general_cfg['which_forecast'] == 'SPV3', 'ISTF forecasts not supported at the moment'
+    
+    def check_which_forecast(self):
+        assert self.covariance_cfg['Spaceborne_cfg']['cl_integral_convention'] in ['PySSC', 'Euclid'], 'cl_integral_convention must be "PySSC" or "Euclid"'        
 
     def run_all_checks(self):
         k_txt_label, pk_txt_label = self.check_h_units()

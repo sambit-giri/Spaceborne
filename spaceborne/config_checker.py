56d7833cb516ffd25fc2d6acf0a656b5a4b3314e
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-10-11 17:06:30 +0200

    add cfg checker

--- spaceborne/config_checker.py
+++ spaceborne/config_checker.py
@@ -31,7 +31,15 @@ class SpaceborneConfigChecker:
         assert self.general_cfg['which_forecast'] == 'SPV3', 'ISTF forecasts not supported at the moment'
     
     def check_which_forecast(self):
-        assert self.covariance_cfg['Spaceborne_cfg']['cl_integral_convention'] in ['PySSC', 'Euclid'], 'cl_integral_convention must be "PySSC" or "Euclid"'        
+        assert self.covariance_cfg['Spaceborne_cfg']['cl_integral_convention'] in ['PySSC', 'Euclid', 'Euclid_KE_approximation'], \
+            'cl_integral_convention must be "PySSC" or "Euclid" or "Euclid_KE_approximation"'        
+
+    def check_types(self):
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['include_b2g'], bool), 'include_b2 must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['b2g_from_halomodel'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['use_KE_approximation'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['load_precomputed_sigma2'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['normalize_shifted_nz'], bool), 'b2g_from_halomodel must be a boolean'
 
     def run_all_checks(self):
         k_txt_label, pk_txt_label = self.check_h_units()
@@ -39,4 +47,5 @@ class SpaceborneConfigChecker:
         self.check_BNT_transform()
         self.check_fsky()
         self.check_which_forecast()
+        self.check_types()
         return k_txt_label, pk_txt_label

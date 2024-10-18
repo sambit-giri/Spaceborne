82d1baf6b11e26e325ebc03761b10a0d5567470f
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-10-18 18:15:49 +0200

    pull from develop

--- spaceborne/config_checker.py
+++ spaceborne/config_checker.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 class SpaceborneConfigChecker:
-    
     def __init__(self, cfg):
         self.general_cfg = cfg['general_cfg']
         self.covariance_cfg = cfg['covariance_cfg']
@@ -34,30 +33,24 @@ class SpaceborneConfigChecker:
         assert self.general_cfg['which_forecast'] == 'SPV3', 'ISTF forecasts not supported at the moment'
 
     def check_which_forecast(self):
-        assert self.covariance_cfg['Spaceborne_cfg']['cl_integral_convention'] in [
-            'PySSC', 'Euclid'], 'cl_integral_convention must be "PySSC" or "Euclid"'
+        assert self.covariance_cfg['Spaceborne_cfg']['cl_integral_convention'] in ['PySSC', 'Euclid', 'Euclid_KE_approximation'], \
+            'cl_integral_convention must be "PySSC" or "Euclid" or "Euclid_KE_approximation"'
 
-    def check_z_grid_ssc_integrands(self):
-        assert self.covariance_cfg['Spaceborne_cfg']['z_steps_ssc_integrands'] >= 250, \
-            'z_steps_ssc_integrands is small, at the moment it used to compute various intermediate quantities'
-            
-    def check_nuisance(self):
-        # ! some check on the input nuisance values
-        # assert np.all(np.array(covariance_cfg['ngal_lensing']) <
-        # 9), 'ngal_lensing values are likely < 9 *per bin*; this is just a rough check'
-        # assert np.all(np.array(covariance_cfg['ngal_clustering']) <
-        # 9), 'ngal_clustering values are likely < 9 *per bin*; this is just a rough check'
-        assert np.all(np.array(self.covariance_cfg['ngal_lensing']) > 0), 'ngal_lensing values must be positive'
-        assert np.all(np.array(self.covariance_cfg['ngal_clustering']) > 0), 'ngal_clustering values must be positive'
-        assert np.all(np.array(self.covariance_cfg['zbin_centers']) > 0), 'z_center values must be positive'
-        assert np.all(np.array(self.covariance_cfg['zbin_centers']) < 3), 'z_center values are likely < 3; this is just a rough check'
-        assert self.general_cfg['magcut_source'] == 245, 'magcut_source should be 245, only magcut lens is varied'
+    def check_types(self):
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['include_b2g'], bool), 'include_b2 must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']['b2g_from_halomodel'],
+                          bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']
+                          ['use_KE_approximation'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['Spaceborne_cfg']
+                          ['load_precomputed_sigma2'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.covariance_cfg['normalize_shifted_nz'], bool), 'b2g_from_halomodel must be a boolean'
+        assert isinstance(self.general_cfg['save_outputs_as_test_benchmarks_path'], (str, bool)
+                          ), 'save_outputs_as_test_benchmarks_path must be either a path (str) or a boolean'
 
-
-    def test_ell_values(self):
-        assert self.general_cfg['nbl_WL_opt'] == 32, 'this is used as the reference binning, from which the cuts are made'
-        assert self.general_cfg['ell_max_WL_opt'] == 5000, 'this is used as the reference binning, from which the cuts are made'
-        assert self.general_cfg['n_probes'] == 2, 'The code can only accept 2 probes at the moment'
+        if isinstance(self.general_cfg['save_outputs_as_test_benchmarks_path'], bool):
+            assert not self.general_cfg['save_outputs_as_test_benchmarks_path'], 'if boolean, '\
+                'save_outputs_as_test_benchmarks_path must be False'
 
     def run_all_checks(self):
         k_txt_label, pk_txt_label = self.check_h_units()
@@ -65,7 +58,5 @@ class SpaceborneConfigChecker:
         self.check_BNT_transform()
         self.check_fsky()
         self.check_which_forecast()
-        self.check_z_grid_ssc_integrands()
-        self.check_nuisance()
-        self.test_ell_values()
+        self.check_types()
         return k_txt_label, pk_txt_label

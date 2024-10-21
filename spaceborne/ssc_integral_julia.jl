ff5fc9dd558009ce5c8cce12b33cd9c786f63d65
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-10-21 11:06:14 +0200

    add SSC_integral_KE_4D_simps. more general input paths

--- spaceborne/ssc_integral_julia.jl
+++ spaceborne/ssc_integral_julia.jl
@@ -132,6 +132,44 @@ function SSC_integral_4D_simps(d2ClAB_dVddeltab, d2ClCD_dVddeltab, ind_AB, ind_C
 end
 
 
+function SSC_integral_KE_4D_simps(d2ClAB_dVddeltab, d2ClCD_dVddeltab, ind_AB, ind_CD, nbl, z_steps, cl_integral_prefactor, sigma2, z_array::Array)
+    """ this version takes advantage of the symmetries between redshift pairs, and implements the KE approximation
+    (see )
+    """
+
+    simpson_weights = get_simpson_weights(length(z_array))
+    z_step = (last(z_array)-first(z_array)) /(length(z_array)-1)
+
+
+    zpairs_AB = size(ind_AB, 1)
+    zpairs_CD = size(ind_CD, 1)
+    num_col = size(ind_AB, 2)
+
+    result = zeros(nbl, nbl, zpairs_AB, zpairs_CD)
+
+    # @tturbo for ell1 in 1:nbl
+    for ell1 in 1:nbl
+        for ell2 in 1:nbl  # this could be further optimized by computing only upper triangular ells (for LLLL, GLGL, GGGG only), but not with tturbo
+            for zij in 1:zpairs_AB
+                for zkl in 1:zpairs_CD
+                    for z_idx in 1:z_steps  # this is the integration variable
+
+                        zi, zj, zk, zl = ind_AB[zij, num_col - 1], ind_AB[zij, num_col], ind_CD[zkl, num_col - 1], ind_CD[zkl, num_col]
+
+                        result[ell1, ell2, zij, zkl] += cl_integral_prefactor[z_idx]*
+                        d2ClAB_dVddeltab[ell1, zi, zj, z_idx] *
+                        d2ClCD_dVddeltab[ell2, zk, zl, z_idx] * sigma2[z_idx] *
+                        simpson_weights[z_idx]
+
+                    end
+                end
+            end
+        end
+    end
+    return result .* z_step
+end
+
+
 # function SSC_integral_4D_opmpson_(d2ClAB_dVddeltab, d2ClCD_dVddeltab, ind_AB, ind_CD, nbl, z_steps, cl_integral_prefactor, sigma2, z_array::Array)
 #     """ this version tries to use the KE approximation, to check its impact on the results.
 #     """
@@ -176,14 +214,14 @@ integration_type = ARGS[2]
 
 # import arrays:
 # the ones actually used in the integration
-d2CLL_dVddeltab = npzread("./$(folder_name)/d2CLL_dVddeltab.npy")
-d2CGL_dVddeltab = npzread("./$(folder_name)/d2CGL_dVddeltab.npy")
-d2CGG_dVddeltab = npzread("./$(folder_name)/d2CGG_dVddeltab.npy")
-sigma2          = npzread("./$(folder_name)/sigma2.npy")
-z_grid = npzread("./$(folder_name)/z_grid.npy") #previously z_integrands
-cl_integral_prefactor = npzread("./$(folder_name)/cl_integral_prefactor.npy")
-ind_auto = npzread("./$(folder_name)/ind_auto.npy")
-ind_cross = npzread("./$(folder_name)/ind_cross.npy")
+d2CLL_dVddeltab = npzread("$(folder_name)/d2CLL_dVddeltab.npy")
+d2CGL_dVddeltab = npzread("$(folder_name)/d2CGL_dVddeltab.npy")
+d2CGG_dVddeltab = npzread("$(folder_name)/d2CGG_dVddeltab.npy")
+sigma2          = npzread("$(folder_name)/sigma2.npy")
+z_grid = npzread("$(folder_name)/z_grid.npy") #previously z_integrands
+cl_integral_prefactor = npzread("$(folder_name)/cl_integral_prefactor.npy")
+ind_auto = npzread("$(folder_name)/ind_auto.npy")
+ind_cross = npzread("$(folder_name)/ind_cross.npy")
 nbl = size(d2CLL_dVddeltab, 1)
 zbins = size(d2CLL_dVddeltab, 2)
 
@@ -201,20 +239,21 @@ ind_cross = ind_cross .+ 1
 # this is for the 3x2pt covariance
 probe_combinations = (("L", "L"), ("G", "L"), ("G", "G"))
 
-println("\n*** Computing SSC integral with Julia ****")
+println("\n*** Computing SSC integral in Julia ****")
+println("specs:")
 println("nbl: ", nbl)
 println("zbins: ", zbins)
 println("z_steps: ", z_steps)
 println("probe_combinations: ", probe_combinations)
 println("integration_type: ", integration_type)
-println("*****************")
+println("*****************\n")
 
 # some sanity checks
 @assert length(z_grid) == z_steps
 @assert size(d2CLL_dVddeltab) == (nbl, zbins, zbins, z_steps)
 @assert size(d2CGL_dVddeltab) == (nbl, zbins, zbins, z_steps)
 @assert size(d2CGG_dVddeltab) == (nbl, zbins, zbins, z_steps)
-@assert size(sigma2) == (z_steps, z_steps)
+# @assert size(sigma2) == (z_steps, z_steps)
 @assert size(cl_integral_prefactor) == (z_steps,)
 @assert size(ind_auto) == (zbins*(zbins+1)/2, num_col)
 @assert size(ind_cross) == (zbins^2, num_col)
@@ -231,6 +270,8 @@ if integration_type == "trapz"
     ssc_integral_4d_func = SSC_integral_4D_trapz
 elseif integration_type == "simps"
     ssc_integral_4d_func = SSC_integral_4D_simps
+elseif integration_type == "simps_KE_approximation"
+    ssc_integral_4d_func = SSC_integral_KE_4D_simps
 elseif integration_type == "trapz-6D"
     ssc_integral_4d_func = SSC_integral_6D_trapz
 else
@@ -250,7 +291,7 @@ for row in 1:length(probe_combinations)
         probe_C, probe_D = probe_combinations[col]
 
         if col >= row  # upper triangle and diagonal
-            println("Computing cov_SSC_$(probe_A)$(probe_B)_$(probe_C)$(probe_D), zbins = $(zbins)")
+            println("Computing SSC covariance block $(probe_A)$(probe_B)_$(probe_C)$(probe_D)")
 
             cov_ssc_dict_8d[(probe_A, probe_B, probe_C, probe_D)] =
             @time ssc_integral_4d_func(
@@ -262,7 +303,7 @@ for row in 1:length(probe_combinations)
                 sigma2, z_grid)
 
             # save
-            npzwrite("./$(folder_name)/cov_SSC_spaceborne_$(probe_A)$(probe_B)$(probe_C)$(probe_D)_4D.npy", cov_ssc_dict_8d[(probe_A, probe_B, probe_C, probe_D)])
+            npzwrite("$(folder_name)/cov_SSC_spaceborne_$(probe_A)$(probe_B)$(probe_C)$(probe_D)_4D.npy", cov_ssc_dict_8d[(probe_A, probe_B, probe_C, probe_D)])
 
             # free memory
             delete!(cov_ssc_dict_8d, (probe_A, probe_B, probe_C, probe_D))

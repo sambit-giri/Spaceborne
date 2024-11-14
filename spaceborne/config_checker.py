e41a50392a6b5bb688a8973b743edfa4347e905b
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-11-14 19:21:39 +0100

    added many checks

--- spaceborne/config_checker.py
+++ spaceborne/config_checker.py
@@ -16,6 +16,7 @@ class SpaceborneConfigChecker:
     def check_ell_cuts(self):
         if self.cfg['ell_cuts']['apply_ell_cuts']:
             assert self.cfg['BNT']['BNT_transform'], 'You should BNT transform if you want to apply ell cuts.'
+            assert self.cfg['ell_cuts']['which_cuts'] == 'standard', 'Other types of cuts not finished to implement'
 
     def check_BNT_transform(self):
         if self.cfg['BNT']['cov_BNT_transform']:
@@ -26,10 +27,22 @@ class SpaceborneConfigChecker:
         assert np.abs(mm.percent_diff(self.cfg['covariance']['fsky'],
                       fsky_check)) < 1e-5, 'Fsky does not match the survey area.'
 
-
     def check_cl_integral_convention(self):
         assert self.cfg['covariance']['cl_integral_convention'] in ['PySSC', 'Euclid', 'Euclid_KE_approximation'], \
             'cl_integral_convention must be "PySSC" or "Euclid" or "Euclid_KE_approximation"'
+
+        if self.cfg['covariance']['use_KE_approximation'] and self.cfg['covariance']['SSC_code'] == 'Spaceborne':
+            assert self.cfg['covariance']['cl_integral_convention'] == 'Euclid_KE_approximation'
+            assert self.cfg['covariance']['integration_type'] == 'simps_KE_approximation'
+            assert self.cfg['covariance']['which_sigma2_b'] not in [None, 'full_curved_sky'], \
+                'to use the flat-sky sigma2_b, set "flat_sky" in the cfg file. Also, bear in mind that the flat-sky '\
+                'approximation for sigma2_b is likely inappropriate for the large Euclid survey area'
+
+        elif not self.cfg['covariance']['use_KE_approximation'] and self.cfg['covariance']['SSC_code'] == 'Spaceborne':
+            assert self.cfg['covariance']['cl_integral_convention'] in ('Euclid', 'PySSC')
+            assert self.cfg['covariance']['integration_type'] in ('simps', 'trapz')
+            assert self.cfg['covariance']['which_sigma2_b'] not in [None, 'flat_sky'], \
+                'If you\'re not using the KE approximation, you should set "full_curved_sky", "from_input_mask or "polar_cap_on_the_fly"'
 
     def check_types(self):
         assert isinstance(self.cfg['covariance']['include_b2g'], bool), 'include_b2 must be a boolean'
@@ -47,6 +60,35 @@ class SpaceborneConfigChecker:
             assert not self.cfg['misc']['save_outputs_as_test_benchmarks_path'], 'if boolean, '\
                 'save_outputs_as_test_benchmarks_path must be False'
 
+    def check_ell_binning(self):
+        assert self.cfg['ell_binning']['nbl_WL_opt'] == 32, 'this is used as the reference binning, from which the cuts are made'
+        assert self.cfg['ell_binning']['ell_max_WL_opt'] == 5000, 'this is used as the reference binning, from which the cuts are made'
+        assert (self.cfg['ell_binning']['ell_max_WL'], self.cfg['ell_binning']['ell_max_GC']) == (5000, 3000) or (1500, 750), \
+            'ell_max_WL and ell_max_GC must be either (5000, 3000) or (1500, 750)'
+
+    def check_misc(self):
+        assert self.cfg['covariance']['n_probes'] == 2, 'The code can only accept 2 probes at the moment'
+
+    def check_nz(self):
+        assert np.all(np.array(self.cfg['nz']['ngal_sources']) > 0), 'ngal_sources values must be positive'
+        assert np.all(np.array(self.cfg['nz']['ngal_lenses']) > 0), 'ngal_lenses values must be positive'
+        assert np.all(self.cfg['nz']['dzWL'] == self.cfg['nz']['dzGC']), 'dzWL and dzGC shifts do not match'
+        assert len(self.cfg['nz']['ngal_sources']) == len(self.cfg['nz']['ngal_lenses']) == \
+            len(self.cfg['nz']['dzWL']) == len(self.cfg['nz']['dzGC'])
+        assert isinstance(self.cfg['nz']['ngal_sources'], list), 'n_gal_shear must be a list'
+        assert isinstance(self.cfg['nz']['ngal_lenses'], list), 'n_gal_clust must be a list'
+
+    def check_cosmo(self):
+        if 'logT' in self.cfg['cosmology']:
+            assert self.cfg['cosmology']['logT'] == self.cfg['extra_parameters']['camb']['HMCode_logT_AGN'], (
+                'Value mismatch for logT_AGN in the parameters definition')
+
+    def check_cov(self):
+        assert self.cfg['covariance']['ep_or_ed'] in ('EP', 'ED'), 'EP_or_ED must be either EP or ED'
+        assert self.cfg['covariance']['triu_tril'] in ('triu', 'tril'), 'triu_tril must be either "triu" or "tril"'
+        assert self.cfg['covariance']['row_col_major'] in ('row-major', 'col-major'), 'row_col_major must be either "row-major" or "col-major"'
+
+
     def run_all_checks(self):
         k_txt_label, pk_txt_label = self.check_h_units()
         self.check_ell_cuts()
@@ -54,4 +96,8 @@ class SpaceborneConfigChecker:
         self.check_cl_integral_convention()
         self.check_fsky()
         self.check_types()
+        self.check_ell_binning()
+        self.check_misc()
+        self.check_nz()
+        self.check_cosmo()
         return k_txt_label, pk_txt_label

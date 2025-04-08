9efc0299ffad36ed378fd323cc226f5456e93798
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-04-08 16:12:38 +0200

    add docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,40 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Spaceborne'
+copyright = '2025, Davide Sciotti'
+author = 'Davide Sciotti'
 
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',      # Include documentation from docstrings
+    'sphinx.ext.napoleon',     # Support for Google and NumPy style docstrings
+    'sphinx.ext.viewcode',     # Add links to source code from documentation
+    'sphinx.ext.intersphinx',  # Link to other projects' docs (e.g. Python)
+    'sphinx_rtd_theme',        # Use the Read the Docs theme
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+
+# Add this block near the top of the file
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..')) # Points to the root of your repo
+                                             # where the 'spaceborne' package lives

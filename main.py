97ceab802ca66ccf5b79896c8c333e89940bd9ae
Davide Sciotti <davidesciotti@gmail.com>
2025-04-18 12:29:23 +0200

    restore

--- main.py
+++ main.py
@@ -1424,30 +1424,6 @@ if cfg['misc']['save_output_as_benchmark']:
         metadata=metadata,
     )
 
-cov = cov_dict['cov_3x2pt_g_2D'] +  cov_dict['cov_3x2pt_ssc_2D'] + cov_dict['cov_3x2pt_cng_2D']
-cov_inv = np.linalg.inv(cov)
-
-# test simmetry
-sl.compare_arrays(
-    cov, cov.T, 'cov', 'cov.T', abs_val=True, log_diff=False, plot_diff_threshold=1
-)
-
-identity = cov @ cov_inv
-identity_true = np.eye(cov.shape[0])
-
-tol = 1e-4
-mask = np.abs(identity) < tol
-masked_identity = np.ma.masked_where(mask, identity)
-sl.matshow(
-    masked_identity, abs_val=True, title=f'cov @ cov_inv\n mask below {tol}', log=True
-)
-
-sl.compare_arrays(
-    cov @ cov_inv, cov_inv @ cov, 'cov @ cov_inv', 'cov_inv @ cov', 
-    abs_val=True, log_diff=True, plot_diff_threshold=1
-)
-
-plt.semilogy(np.linalg.eigvals(cov))
 
 for which_cov in cov_dict:
     if '3x2pt' in which_cov and 'tot' in which_cov:

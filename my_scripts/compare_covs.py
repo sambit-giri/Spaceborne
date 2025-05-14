097067551ed41cad480addc055a1a590c860a204
Davide Sciotti <davidesciotti@gmail.com>
2025-05-14 16:22:08 +0200

    update with actual comparisons

--- my_scripts/compare_covs.py
+++ my_scripts/compare_covs.py
@@ -1,43 +1,66 @@
 """Simple script to run different tests and comparison on two covariance matrices.
-11/04/2025: As of now, I just dumped some code not to lose it, no real development yet.
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 from spaceborne import sb_lib as sl
 
+common_path = '/home/cosmo/davide.sciotti/data/Spaceborne/output'
+cov_a = np.load(f'{common_path}/nmt_cov_test/cov_G_3x2pt_2D.npz')['arr_0']
+cov_b = np.load(f'{common_path}/sample_cov_test/cov_G_3x2pt_2D.npz')['arr_0']
 
 
-cov = (
-    cov_dict['cov_3x2pt_g_2D']
-    + cov_dict['cov_3x2pt_ssc_2D']
-    + cov_dict['cov_3x2pt_cng_2D']
-)
-cov_inv = np.linalg.inv(cov)
+# # test simmetry
+# sl.compare_arrays(
+#     cov, cov.T, 'cov', 'cov.T', abs_val=True, log_diff=False, plot_diff_threshold=1
+# )
 
-# test simmetry
+# identity = cov @ cov_inv
+# identity_true = np.eye(cov.shape[0])
+
+# tol = 1e-4
+# mask = np.abs(identity) < tol
+# masked_identity = np.ma.masked_where(mask, identity)
+# sl.matshow(
+#     masked_identity, abs_val=True, title=f'cov @ cov_inv\n mask below {tol}', log=True
+# )
+
+# visual comparison: covariance
 sl.compare_arrays(
-    cov, cov.T, 'cov', 'cov.T', abs_val=True, log_diff=False, plot_diff_threshold=1
+    cov_a,
+    cov_b,
+    'cov nmt',
+    'cov sample',
+    log_array=True,
+    abs_val=False,
+    log_diff=False,
+    plot_diff_threshold=10,
 )
 
-identity = cov @ cov_inv
-identity_true = np.eye(cov.shape[0])
+# visual comparison: correlation
+corr_a = sl.cov2corr(cov_a)
+corr_b = sl.cov2corr(cov_b)
+sl.plot_correlation_matrix(corr_a)
+sl.plot_correlation_matrix(corr_b)
 
-tol = 1e-4
-mask = np.abs(identity) < tol
-masked_identity = np.ma.masked_where(mask, identity)
-sl.matshow(
-    masked_identity, abs_val=True, title=f'cov @ cov_inv\n mask below {tol}', log=True
+# main diagonal
+sl.compare_funcs(
+    x=None,
+    y={
+        'nmt': np.diag(cov_a),
+        'sample': np.diag(cov_b),
+    },
+    logscale_y=(True, False),
+    title='diag',
 )
 
-sl.compare_arrays(
-    cov @ cov_inv,
-    cov_inv @ cov,
-    'cov @ cov_inv',
-    'cov_inv @ cov',
-    abs_val=True,
-    log_diff=True,
-    plot_diff_threshold=1,
+# spectrum
+sl.compare_funcs(
+    x=None,
+    y={
+        'diag nmt': np.linalg.eigvals(cov_a),
+        'diag sample': np.linalg.eigvals(cov_b),
+    },
+    logscale_y=(True, False),
+    title='spectrum',
 )
-
-plt.semilogy(np.linalg.eigvals(cov))
